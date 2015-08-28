4529f1964adedd8071420c88b0e1a4b77a433b52
Zip Lee <54459708@qq.com>
2015-08-28 14:22:31 +0800

    Update ImageAction.swift

--- ImagePickerSheetController/ImagePickerSheetController/ImageAction.swift
+++ ImagePickerSheetController/ImagePickerSheetController/ImageAction.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-public enum ImageActionStyle {
+@objc public enum ImageActionStyle : Int {
     case Default
     case Cancel
 }
 
 public typealias Title = Int -> String
 
-public class ImageAction {
+@objc public class ImageAction : NSObject{
     
     public typealias Handler = (ImageAction) -> ()
     public typealias SecondaryHandler = (ImageAction, Int) -> ()
@@ -36,24 +36,27 @@ public class ImageAction {
     /// Secondary title defaults to title if not specified.
     /// Secondary Handler defaults to handler if both, the secondary handler and secondary title are not specified.
     public convenience init(title: String, secondaryTitle: String? = nil, style: ImageActionStyle = .Default, handler: Handler? = nil, secondaryHandler: SecondaryHandler? = nil) {
-        self.init(title: title, secondaryTitle: secondaryTitle.map { string in { _ in string }}, style: style, handler: handler, secondaryHandler: secondaryHandler)
+        self.init(primaryTitle: title, secondaryTitle: secondaryTitle.map { string in { _ in string }}, style: style, handler: handler, secondaryHandler: secondaryHandler)
     }
     
     /// Initializes a new ImageAction. The secondary title and handler are used when at least 1 image has been selected.
     /// Secondary title defaults to title if not specified. Use the closure to format a title according to the selection.
     /// Secondary Handler defaults to handler if both, the secondary handler and secondary title are not specified.
-    public init(title: String, secondaryTitle: Title?, style: ImageActionStyle = .Default, handler: Handler? = nil, var secondaryHandler: SecondaryHandler? = nil) {
-        if let handler = handler where secondaryTitle == nil && secondaryHandler == nil {
-            secondaryHandler = { action, _ in
-                handler(action)
-            }
-        }
-        
-        self.title = title
-        self.secondaryTitle = secondaryTitle ?? { _ in title }
+    public init(primaryTitle: String, secondaryTitle: Title?, style: ImageActionStyle = .Default, handler: Handler? = nil, secondaryHandler: SecondaryHandler? = nil) {
+       
+        self.title = primaryTitle
+        self.secondaryTitle = secondaryTitle ?? { _ in primaryTitle }
         self.style = style
         self.handler = handler
-        self.secondaryHandler = secondaryHandler
+        if let handler = handler where secondaryTitle == nil && secondaryHandler == nil {
+            self.secondaryHandler = { action, _ in
+                handler(action)
+            }
+            
+        }else
+        {
+            self.secondaryHandler = secondaryHandler
+        }
     }
     
     func handle(numberOfImages: Int = 0) {
